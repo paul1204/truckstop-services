7168b61afdc458ad6126c56c5f1511b6b5b36ac0
Paul <paulsingh4118@gmail.com>
2024-07-22 23:07:49 -0400

    Added more Test inventory

--- src/main/java/com/truckstopservices/inventory/manager/config/InventoryConfig.java
+++ src/main/java/com/truckstopservices/inventory/manager/config/InventoryConfig.java
@@ -17,16 +17,17 @@ public class InventoryConfig {
     @Bean
     public InitialInventory initalInventory(){
         List<Snack> initalSnacks = Arrays.asList(
-                new Chips("123", "Plain", 1.49, Brands.LAYS),
-                new Chips("456", "Spicy", 1.49, Brands.LAYS),
-                new Chips("789", "Cool Ranch", 1.49, Brands.DORITOS)
+                new Chips("123", "Plain", 1.49, Brands.LAYS,5),
+                new Chips("456", "Spicy", 1.49, Brands.LAYS,5),
+                new Chips("789", "Cool Ranch", 1.49, Brands.DORITOS,5)
                 // Add more predefined snacks as needed
         );
         List<Beverage> initalDrinks = Arrays.asList(
-        new Beverage("B123", "Coca Cola", "12oz", 1.49, Brands.COCACOLA),
-        new Beverage("B456", "Diet Coke", "12oz", 1.49, Brands.COCACOLA),
-        new Beverage("B789", "Sprite", "12oz", 1.49, Brands.COCACOLA),
-        new Beverage("B123", "Sprite Zero", "12oz", 1.49, Brands.COCACOLA)
+        new Beverage("B123", "Coca Cola", "12oz", 1.49, Brands.COCACOLA,5),
+        new Beverage("B456", "Diet Coke", "12oz", 1.49, Brands.COCACOLA,5),
+        new Beverage("B789", "Sprite", "12oz", 1.49, Brands.COCACOLA,5),
+        new Beverage("B123", "Sprite Zero", "12oz", 1.49, Brands.COCACOLA,5),
+        new Beverage("B963", "Vanilla Coke", "12oz", 1.59, Brands.COCACOLA,5)
                 // Add more predefined snacks as needed
         );
         return new InitialInventory(initalSnacks, initalDrinks);

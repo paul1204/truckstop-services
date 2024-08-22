631550b50d66dad0a51e999dbc01b2cb437cec2e
Paul <paulsingh4118@gmail.com>
2024-08-21 20:03:35 -0400

    Initalized Non Restaurant Inventory

---
+++ src/main/java/com/truckstopservices/inventory/merchandise/config/MerchandiseInventoryInitalizer.java
@@ -0,0 +1,33 @@
+package com.truckstopservices.inventory.merchandise.config;
+
+import com.truckstopservices.inventory.fuel.entity.Diesel;
+import com.truckstopservices.inventory.fuel.entity.MidGradeOctane;
+import com.truckstopservices.inventory.fuel.entity.PremiumOctane;
+import com.truckstopservices.inventory.fuel.entity.RegularOctane;
+import com.truckstopservices.inventory.merchandise.beverages.entity.ColdBeverage;
+import com.truckstopservices.inventory.merchandise.nonRestaurant.entity.NonRestaurantFood;
+import com.truckstopservices.inventory.merchandise.repository.BeverageRepository;
+import com.truckstopservices.inventory.merchandise.repository.NonRestaurant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+public class MerchandiseInventoryInitalizer {
+
+    @Autowired
+    BeverageRepository beverageRepository;
+
+    @Autowired
+    NonRestaurant nonRestaurantRepository;
+
+    @Bean
+    public CommandLineRunner loadFuelInventoryData() {
+        return args -> {
+        beverageRepository.save(new ColdBeverage("100", "Coke", 1.99, "Coca-Cola", 20));
+        beverageRepository.save(new ColdBeverage("101", "Diet Coke", 1.99, "Coca-Cola", 20));
+
+        nonRestaurantRepository.save(new NonRestaurantFood("500", "Chips", 1.00, "Lays", 20));
+        nonRestaurantRepository.save(new NonRestaurantFood("501", "Spicy Chips", 1.00, "Lays", 20));
+        };
+    }
+}

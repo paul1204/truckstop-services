be9d5e28a5434504e4f41e533f878bf4d739f29a
Paul <paulsingh4118@gmail.com>
2024-09-03 22:34:03 -0400

    Added more inital items to inventory

--- src/main/java/com/truckstopservices/inventory/merchandise/config/MerchandiseInventoryInitalizer.java
+++ src/main/java/com/truckstopservices/inventory/merchandise/config/MerchandiseInventoryInitalizer.java
@@ -23,9 +23,15 @@ public class MerchandiseInventoryInitalizer {
         return args -> {
         beverageRepository.save(new ColdBeverage("100", "Coke", 1.99, "Coca-Cola", 20, "20oz"));
         beverageRepository.save(new ColdBeverage("101", "Diet Coke", 1.99, "Coca-Cola", 20, "20oz"));
+        beverageRepository.save(new ColdBeverage("102", "Sprite", 1.99, "Coca-Cola", 20, "20oz"));
+        beverageRepository.save(new ColdBeverage("103", "Ginger Ale", 1.99, "Coca-Cola", 20, "20oz"));
+        beverageRepository.save(new ColdBeverage("104", "Sprite Zero", 1.99, "Coca-Cola", 20, "20oz"));
 
         nonRestaurantRepository.save(new NonRestaurantFood("999", "Chips", 1.00, "Lays", 20, "M"));
         nonRestaurantRepository.save(new NonRestaurantFood("998", "Spicy Chips", 1.00, "Lays", 20, "M"));
+        nonRestaurantRepository.save(new NonRestaurantFood("997", "Cookies", 1.00, "Little Debbie", 20, "12pk"));
+        nonRestaurantRepository.save(new NonRestaurantFood("996", "Donut", 1.00, "Little Debbie", 20, "6pk"));
+        nonRestaurantRepository.save(new NonRestaurantFood("995", "Reese's Cup", 1.00, "Hershey", 20, "M"));
         };
     }
 }

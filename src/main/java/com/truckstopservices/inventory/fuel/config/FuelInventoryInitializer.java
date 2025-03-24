ab38ac3611ff9ef98a6d66fb43a01d39377d2edf
Paul <paulsingh4118@gmail.com>
2025-03-23 21:49:29 -0400

    Commenting out Pre Loaded Fuel Inventory, not sure if needed for future use

--- src/main/java/com/truckstopservices/inventory/fuel/config/FuelInventoryInitializer.java
+++ src/main/java/com/truckstopservices/inventory/fuel/config/FuelInventoryInitializer.java
@@ -33,10 +33,10 @@ public class FuelInventoryInitializer {
     @Bean
     public CommandLineRunner loadFuelInventoryData() {
         return args -> {
-            dieselRepository.save(new Diesel("02/24/2025",40, 2.99, 100));
-            regularFuelRepository.save(new RegularOctane("02/24/2025",87, baseGasPrice ,100));
-            midGradeFuelRepository.save(new MidGradeOctane("02/24/2025",89, (baseGasPrice + 0.25), 100));
-            premimumFuelRepository.save(new PremiumOctane("02/24/2025",91, (baseGasPrice + 0.50),100));
+//            dieselRepository.save(new Diesel("02/24/2025",40, 2.99, 100));
+//            regularFuelRepository.save(new RegularOctane("02/24/2025",87, baseGasPrice ,100));
+//            midGradeFuelRepository.save(new MidGradeOctane("02/24/2025",89, (baseGasPrice + 0.25), 100));
+//            premimumFuelRepository.save(new PremiumOctane("02/24/2025",91, (baseGasPrice + 0.50),100));
         };
     }
 }

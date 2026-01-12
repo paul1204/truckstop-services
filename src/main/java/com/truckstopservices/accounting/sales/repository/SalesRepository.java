9bc102d9aeee45d527c27760b712b49f8c6ea9d4
Paramveer Singh <46756848+paul1204@users.noreply.github.com>
2026-01-11 19:07:01 -0500

    fix: rounding to 2 decimal places for sales_amount in query

--- src/main/java/com/truckstopservices/accounting/sales/repository/SalesRepository.java
+++ src/main/java/com/truckstopservices/accounting/sales/repository/SalesRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, String> {
 
-    @Query(value = "SELECT SUM(sales_amount) AS salesAmount, shift_number AS shiftNumber FROM sales GROUP BY shift_number ORDER BY shift_number ASC", nativeQuery = true)
+    @Query(value = "SELECT ROUND(SUM(sales_amount),2) AS salesAmount, shift_number AS shiftNumber FROM sales GROUP BY shift_number ORDER BY shift_number ASC", nativeQuery = true)
     List<SalesByShift> findSalesByTodayAllShifts();
 }

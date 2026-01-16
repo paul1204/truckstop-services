4205b207edbda92ac73691148a90df570c0db6fe
Paramveer Singh <46756848+paul1204@users.noreply.github.com>
2026-01-16 17:29:54 -0500

    fix: need to include MidGradeOctane.java to reflect new column name change

--- src/main/java/com/truckstopservices/inventory/fuel/entity/MidGradeOctane.java
+++ src/main/java/com/truckstopservices/inventory/fuel/entity/MidGradeOctane.java
@@ -4,9 +4,6 @@ import com.truckstopservices.inventory.fuel.model.CalculateFuelPrice;
 import com.truckstopservices.inventory.fuel.model.Fuel;
 import jakarta.persistence.Entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
 public class MidGradeOctane extends Fuel implements CalculateFuelPrice {
@@ -14,12 +11,12 @@ public class MidGradeOctane extends Fuel implements CalculateFuelPrice {
     public MidGradeOctane(){
         super();
     }
-//    public MidGradeOctane(int octane, double pricePerGallon, double initalGallons) {
-//        super(octane, pricePerGallon, initalGallons, new ArrayList<Double>(List.of(pricePerGallon)), new ArrayList<Double>(List.of(initalGallons)));
+//    public MidGradeOctane(int octane, double costPerGallon, double initalGallons) {
+//        super(octane, costPerGallon, initalGallons, new ArrayList<Double>(List.of(costPerGallon)), new ArrayList<Double>(List.of(initalGallons)));
 //    }
 
     @Override
     public double calculateGallonsToDispense(double amount) {
-        return amount / getPricePerGallon();
+        return amount / getCostPerGallon();
     }
 }
